3cfc29f5e70822f670944585ad70cf77ca09a5e2
HerbCaudill <herb@devresults.com>
2021-02-08 13:15:14 +0100

    chain/predecessors.ts:

--- src/chain/predecessors.ts
+++ src/chain/predecessors.ts
@@ -7,36 +7,32 @@ export const getPredecessors = <T extends Action>(
   link: Link<T>
 ): Link<T>[] => {
   const parents = getParents(chain, link)
-  const predecessors = parents.flatMap(parent => getPredecessors(chain, parent))
+  const predecessors = parents.flatMap((parent) => getPredecessors(chain, parent))
   return R.uniq(parents.concat(predecessors))
 }
-
-// TODO make these signatures consistent -
-// chain, [a,b]
 
 /** Returns true if `a` is a predecessor of `b` */
 export const isPredecessor = <T extends Action>(
   chain: SignatureChain<T>,
-  a: Link<T>,
-  b: Link<T>
+  [a, b]: [Link<T>, Link<T>]
 ): boolean => getPredecessors(chain, b).includes(a)
 
 // TODO: probably want to memoize this
 
 export const getCommonPredecessor = <T extends Action = Action>(
   chain: SignatureChain<T>,
-  [a, b]: Link<T>[]
+  [a, b]: [Link<T>, Link<T>]
 ): Link<T> => {
   // are they the same node?
   if (a === b) return a
 
   // does one precede the other?
-  if (isPredecessor(chain, a, b)) return a
-  if (isPredecessor(chain, b, a)) return b
+  if (isPredecessor(chain, [a, b])) return a
+  if (isPredecessor(chain, [b, a])) return b
 
   const aPredecessors = getPredecessors(chain, a)
   const bPredecessors = getPredecessors(chain, b)
-  return aPredecessors.find(link => bPredecessors.includes(link))!
+  return aPredecessors.find((link) => bPredecessors.includes(link))!
 }
 
 export const getParentHashes = (chain: SignatureChain<any>, link: Link<any>) =>
@@ -47,4 +43,4 @@ export const getParentHashes = (chain: SignatureChain<any>, link: Link<any>) =>
     : [link.body.prev] // normal link = 1 parent
 
 export const getParents = (chain: SignatureChain<any>, link: Link<any>) =>
-  getParentHashes(chain, link).map(hash => chain.links[hash])
+  getParentHashes(chain, link).map((hash) => chain.links[hash])
