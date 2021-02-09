4a6654d13ffcba6a3f051254232b59f5f914c847
HerbCaudill <herb@devresults.com>
2021-02-09 11:19:16 +0100

    util/profile.ts: add instructions because I keep forgetting

--- src/util/profile.ts
+++ src/util/profile.ts
@@ -3,6 +3,24 @@ import path from 'path'
 
 const profiler = require('v8-profiler-node8')
 
+/**
+ * To use:
+ *
+ * ```ts
+ * const my_slow_function = () => {...}
+ * await profile(my_slow_function)
+ * ```
+ *
+ * Each time this is run, it will rewrite a file at `./profiles/my_slow_function.cpuprofile`.
+ *
+ * To analyze this file:
+ *
+ * - open Chrome
+ * - visit `chrome://inspect`
+ * - click 'Open dedicated DevTools for Node'
+ * - switch to the 'Profiler' tab
+ * - press 'Load' and find `./profiles/my_slow__function.cpuprofile`
+ */
 export const profile = async (fn: Function) => {
   profiler.startProfiling()
   await fn()
