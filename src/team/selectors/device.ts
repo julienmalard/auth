dd7d91e639e210ade9a0578a4dea54a5ac6c6735
HerbCaudill <herb@devresults.com>
2021-02-10 15:50:09 +0100

    team/selectors/device.ts: create

---
+++ src/team/selectors/device.ts
@@ -0,0 +1,10 @@
+﻿import { member } from './member'
+import { TeamState } from '/team/types'
+
+export const device = (state: TeamState, userName: string, deviceName: string) => {
+  const memberDevices = member(state, userName).devices || []
+  const device = memberDevices.find(d => d.deviceName === deviceName)
+  if (device === undefined)
+    throw new Error(`Member ${userName} does not have a device called ${deviceName}`)
+  return device
+}
