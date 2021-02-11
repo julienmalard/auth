32891a87ea53f5937dd96a58cedbb0815ab99e7c
HerbCaudill <herb@devresults.com>
2021-02-11 09:05:05 +0100

    util/memoize.ts:

--- src/util/memoize.ts
+++ src/util/memoize.ts
@@ -1,7 +1,14 @@
 ﻿import _memoize from 'fast-memoize'
 import { MemoizeFunc } from 'fast-memoize/typings/fast-memoize'
 
+const BYPASS = false
+
+const passthrough = <T>(f: T) => f as T
+
+export const memoize = (BYPASS ? passthrough : _memoize) as Memoize
+
 // types that are not exported from fast-memoize
+
 type Func = (...args: any[]) => any
 
 interface Memoize extends MemoizeFunc {
@@ -10,8 +17,3 @@ interface Memoize extends MemoizeFunc {
     monadic: MemoizeFunc
   }
 }
-
-const BYPASS = false
-const passthrough = <T>(f: T) => f as T
-
-export const memoize = (BYPASS ? passthrough : _memoize) as Memoize
