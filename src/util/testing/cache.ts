b90f29dd1afc0e3f32ef971a4ab78b9feaf5f375
HerbCaudill <herb@devresults.com>
2021-02-11 09:04:48 +0100

    util/testing/cache.ts: make easier to bypass

--- src/util/testing/cache.ts
+++ src/util/testing/cache.ts
@@ -2,8 +2,12 @@ import fs from 'fs'
 import path from 'path'
 import { memoize } from '../memoize'
 
-// TEST ASSETS
+const BYPASS = false
+
+// This is used for caching test assets in the file system to save time
 export const cache = <T>(fileName: string, generateAsset: () => T): T => {
+  if (BYPASS) return generateAsset() // don't use cache
+
   fileName = fileSystemSafe(fileName)
   const filePath = path.join(__dirname, `./assets/${fileName}.json`)
 
@@ -11,10 +15,11 @@ export const cache = <T>(fileName: string, generateAsset: () => T): T => {
   if (fs.existsSync(filePath)) return parseCacheFile(filePath) as T
 
   // otherwise generate the asset
-  const result: any = generateAsset()
+  const result = generateAsset()
   fs.writeFileSync(filePath, JSON.stringify(result))
   return result as T
 }
+
 const fileSystemSafe = (s: string) =>
   s
     .replace(/user/gi, '')
